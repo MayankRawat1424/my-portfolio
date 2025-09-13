74fed95410bd51a7acef8d069d64b0997387dd91
Mayank <mayankrawat1424@gmail.com>
2025-09-13 23:09:13 +0530

    style(Header, HomePage): finish styling and layout

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,25 +2,27 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex">
-      <h1>Mayank R.</h1>
-      <nav>
-        <ul className="flex">
-          <li>
-            <p>Home</p>
-          </li>
-          <li>
-            <p>About</p>
-          </li>
-          <li>
-            <p>Projects</p>
-          </li>
-          <li>
-            <p>Contact</p>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <header className=" sticky top-0 z-50 backdrop-blur-xs px-4">
+      <div className="max-w-7xl flex justify-between items-baseline pt-10 mx-auto pb-5 border-b-1 mb-16">
+        <h1 className="font-bold font-serif">Mayank_R.</h1>
+        <nav>
+          <ul className="flex gap-14 text-sm">
+            <li>
+              <button className="hover:cursor-pointer">Home</button>
+            </li>
+            <li>
+              <button className="hover:cursor-pointer">About</button>
+            </li>
+            <li>
+              <button className="hover:cursor-pointer">Projects</button>
+            </li>
+            <li>
+              <button className="hover:cursor-pointer">Contact</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -1,10 +1,65 @@
 import React from "react";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div>
-      <p>Hi, I'm Mayank Rawat and I am a Web Developer and an ML Enthusiast</p>
-    </div>
+    <>
+      <div className="px-4 flex justify-between align-left max-w-7xl mx-auto mx-4 pb-5 mb-16 ">
+        <div className="">
+          <Image
+            src="/MRawat.jpeg"
+            alt="My Profile Picture"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className="w-3/5 mx-24">
+          <p className="font-bold text-3xl mb-2">Hi, I'm Mayank Rawat</p>
+          <p className="text-3xl mb-6">
+            I love turning ideas into clean, functional, and user-friendly web
+            experiences.
+          </p>
+          <p className="text-xl">
+            I have a strong interest in web development, cloud technologies, and
+            machine learning, and I enjoy exploring how these fields intersect
+            to create impactful solutions. Beyond my technical pursuits, I value
+            clear and effective communication, which enables me to collaborate
+            seamlessly, share ideas with clarity, and contribute meaningfully to
+            any team or project.
+          </p>
+        </div>
+        <div className="">
+          <dl>
+            <div>
+              <dt className="text-lg font-semibold text-gray-900">
+                Web Development
+              </dt>
+              <dd className="text-gray-600">
+                Building responsive and accessible web applications.
+              </dd>
+            </div>
+
+            <div>
+              <dt className="text-lg font-semibold text-gray-900">
+                Cloud Technologies
+              </dt>
+              <dd className="text-gray-600">
+                Deploying and managing applications on cloud platforms.
+              </dd>
+            </div>
+
+            <div>
+              <dt className="text-lg font-semibold text-gray-900">
+                Machine Learning
+              </dt>
+              <dd className="text-gray-600">
+                Implementing ML models to solve real-world problems.
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </>
   );
 };
 
