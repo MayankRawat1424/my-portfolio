05139f6b7245a765100aa4e3e494a51fc79da8d5
Mayank <mayankrawat1424@gmail.com>
2025-09-15 22:07:40 +0530

    hosting on github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization for static export
+  },
+  assetPrefix: isProd ? "/my-portfolio/" : "",
+  basePath: isProd ? "/my-portfolio" : "",
+  output: "export", // Enable static HTML export
+};
 
 export default nextConfig;
